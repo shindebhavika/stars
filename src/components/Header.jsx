ee6c9e0eb162428ae8d81cc79f3b526ce3f379fa
shindebhavika <bhavikashinde74@gmail.com>
2024-02-24 09:49:17 +0530

    up 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <div className="Header h-32  border border-gray-950 w-[100%]  ">
       <header className="flex flex-wrap justify-between items-center py-3 mb-4 mt-3">
-        <div className="flex-shrink-0 w-28 rounded-full ml-6">
+        <div className="flex-shrink-0 w-28 rounded-full ml-6 header-left">
           <Link to="/" className="flex items-center ">
             <img src="https://cdn.dribbble.com/users/4229785/screenshots/14072506/media/4fa4868265580c2792ef24887618511c.jpg" alt="Logo" className="w-full rounded-full" />
           </Link>
